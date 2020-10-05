1204e263ea4988469720f2c9ad2fe819817175ea
Otim Fredrick <fredrickot@gmail.com>
2020-10-05 18:23:10 +0300

    writes tests for audience and message

--- tests/Feature/Subject/AudienceTest.php
+++ tests/Feature/Subject/AudienceTest.php
@@ -36,4 +36,69 @@ class AudienceTest extends TestCase
 
         $this->assertEquals(1, Audience::count());
     }
+
+    /** @test */
+    public function a_congragulation_message_is_required_for_a_message_can_be_created()
+    {
+        $subject = factory(Subject::class)->create([
+            'title' => 'Subject title',
+            'slug' => 'subject_title',
+            'subtitle' => 'subject subtitle',
+            'description' => 'Subject description',
+            'category' => 'Test',
+        ]);
+
+        $audience = factory(Audience::class)->make([
+            'student_learn' => [],
+            'class_requirement' => ['Curriculus'],
+            'target_student' => ['Senior 4', 'Senior 5', 'Senior 6']
+        ]);
+
+        $response = $this->post('/subjects/subject_title/audiences', $audience->toArray());
+
+        $this->assertEquals(302, $response->getStatusCode());
+    }
+
+    /** @test */
+    public function a_class_requirement_is_required_for_a_message_can_be_created()
+    {
+        $subject = factory(Subject::class)->create([
+            'title' => 'Subject title',
+            'slug' => 'subject_title',
+            'subtitle' => 'subject subtitle',
+            'description' => 'Subject description',
+            'category' => 'Test',
+        ]);
+
+        $audience = factory(Audience::class)->make([
+            'student_learn' => ['Maths 1', 'Maths 2'],
+            'class_requirement' => [],
+            'target_student' => ['Senior 4', 'Senior 5', 'Senior 6']
+        ]);
+
+        $response = $this->post('/subjects/subject_title/audiences', $audience->toArray());
+
+        $this->assertEquals(302, $response->getStatusCode());
+    }
+    /** @test */
+    public function a_target_student_is_required_for_a_message_can_be_created()
+    {
+        $subject = factory(Subject::class)->create([
+            'title' => 'Subject title',
+            'slug' => 'subject_title',
+            'subtitle' => 'subject subtitle',
+            'description' => 'Subject description',
+            'category' => 'Test',
+        ]);
+
+        $audience = factory(Audience::class)->make([
+            'student_learn' => ['Maths 1', 'Maths 2'],
+            'class_requirement' => ['Curriculus'],
+            'target_student' => []
+        ]);
+
+        $response = $this->post('/subjects/subject_title/audiences', $audience->toArray());
+
+        $this->assertEquals(302, $response->getStatusCode());
+    }
 }
--- tests/Feature/Subject/MessageTest.php
+++ tests/Feature/Subject/MessageTest.php
@@ -15,8 +15,6 @@ class MessageTest extends TestCase
     /** @test */
     public function a_valid_message_can_be_created()
     {
-        $this->withoutExceptionHandling();
-
         $subject = factory(Subject::class)->create([
             'title' => 'Subject title',
             'slug' => 'subject_title',
@@ -33,5 +31,49 @@ class MessageTest extends TestCase
         $response->assertRedirect("/subjects/subject_title");
 
         $this->assertEquals(1, Message::count());
+    }
+
+    /** @test */
+    public function a_welcome_message_is_required_for_a_message_can_be_created()
+    {
+        $subject = factory(Subject::class)->create([
+            'title' => 'Subject title',
+            'slug' => 'subject_title',
+            'subtitle' => 'subject subtitle',
+            'description' => 'Subject description',
+            'category' => 'Test',
+        ]);
+
+        $message = factory(Message::class)->make([
+            'welcome_message' => '',
+            'congragulation_message' => 'Thank you for taking our course'
+        ]);
+
+        $response = $this->post('/subjects/subject_title/messages', $message->toArray())
+                                            ->assertSessionHasErrors(('welcome_message'));
+
+       $this->assertEquals(0, Message::count());
+    }
+
+    /** @test */
+    public function a_congragulation_message_is_required_for_a_message_can_be_created()
+    {
+        $subject = factory(Subject::class)->create([
+            'title' => 'Subject title',
+            'slug' => 'subject_title',
+            'subtitle' => 'subject subtitle',
+            'description' => 'Subject description',
+            'category' => 'Test',
+        ]);
+
+        $message = factory(Message::class)->make([
+            'welcome_message' => 'Your welcome to our course',
+            'congragulation_message' => ''
+        ]);
+
+        $response = $this->post('/subjects/subject_title/messages', $message->toArray())
+                                            ->assertSessionHasErrors(('congragulation_message'));
+
+       $this->assertEquals(0, Message::count());
     }
 }
