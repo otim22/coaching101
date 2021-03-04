498d1924335a3d7ca6b65f3457c260c08a549945
Otim Fredrick <fredrickot@gmail.com>
2021-03-04 11:11:16 +0300

    filters only approved subjects, books, notes, and pastpapers to be displayed

--- resources/views/student/books/partials/filtered_books.blade.php
+++ resources/views/student/books/partials/filtered_books.blade.php
@@ -1,49 +1,64 @@
 <div class="row" id="subject_data">
     @forelse($books as $book)
-        <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
-            <div class="mb-3">
-                <div class="card">
-                    @if($book->isSubscribedTo)
-                        <a href="{{ route('student.books.show', $book) }}" style="text-decoration: none">
-                            @if($book->creator)
-                                <img src="{{ $book->getFirstMediaUrl('teacher_cover_image') }}" alt="{{ $book->very_short_title }}" width="100%" height="150">
-                            @else
-                                <img src="{{ $book->getFirstMediaUrl('cover_image') }}" alt="{{ $book->very_short_title }}" width="100%" height="150">
-                            @endif
-                        </a>
-                    @elseif(!$book->price)
-                        <a href="{{ route('student.books.show', $book) }}" style="text-decoration: none">
-                            @if($book->creator)
-                                <img src="{{ $book->getFirstMediaUrl('teacher_cover_image') }}" alt="{{ $book->very_short_title }}" width="100%" height="150">
-                            @else
-                                <img src="{{ $book->getFirstMediaUrl('cover_image') }}" alt="{{ $book->very_short_title }}" width="100%" height="150">
-                            @endif
-                        </a>
-                    @else
-                        @if($book->creator)
-                            <img src="{{ $book->getFirstMediaUrl('teacher_cover_image') }}" alt="{{ $book->very_short_title }}" width="100%" height="150">
-                        @else
-                            <img src="{{ $book->getFirstMediaUrl('cover_image') }}" alt="{{ $book->very_short_title }}" width="100%" height="150">
-                        @endif
-                    @endif
-                    <div class="card-body">
+        @if($book->is_approved)
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+                <div class="mb-3">
+                    <div class="card">
                         @if($book->isSubscribedTo)
-                            <a href="{{ route('student.books.show', $book) }}" style="text-decoration: none" class="title-font">
-                                <span class="bold">{{ $book->very_short_title }}</span><br />
+                            <a href="{{ route('student.books.show', $book) }}" style="text-decoration: none">
                                 @if($book->creator)
-                                    <span class="author-font">By {{ $book->creator->name }}</span><br />
+                                    <img src="{{ $book->getFirstMediaUrl('teacher_cover_image') }}" alt="{{ $book->very_short_title }}" width="100%" height="150">
                                 @else
-                                    <span class="author-font">By {{ \App\Constants\GlobalConstants::ADMIN }}</span><br />
-                                @endif
-
-                                @if($book->price)
-                                    UGX {{ number_format($book->price) }}/- <span class="author-font">(Paid)</span>
-                                @else
-                                    <span class="bold paid_color">Free</span>
+                                    <img src="{{ $book->getFirstMediaUrl('cover_image') }}" alt="{{ $book->very_short_title }}" width="100%" height="150">
                                 @endif
                             </a>
                         @elseif(!$book->price)
-                            <a href="{{ route('student.books.show', $book) }}" style="text-decoration: none" class="title-font">
+                            <a href="{{ route('student.books.show', $book) }}" style="text-decoration: none">
+                                @if($book->creator)
+                                    <img src="{{ $book->getFirstMediaUrl('teacher_cover_image') }}" alt="{{ $book->very_short_title }}" width="100%" height="150">
+                                @else
+                                    <img src="{{ $book->getFirstMediaUrl('cover_image') }}" alt="{{ $book->very_short_title }}" width="100%" height="150">
+                                @endif
+                            </a>
+                        @else
+                            @if($book->creator)
+                                <img src="{{ $book->getFirstMediaUrl('teacher_cover_image') }}" alt="{{ $book->very_short_title }}" width="100%" height="150">
+                            @else
+                                <img src="{{ $book->getFirstMediaUrl('cover_image') }}" alt="{{ $book->very_short_title }}" width="100%" height="150">
+                            @endif
+                        @endif
+                        <div class="card-body">
+                            @if($book->isSubscribedTo)
+                                <a href="{{ route('student.books.show', $book) }}" style="text-decoration: none" class="title-font">
+                                    <span class="bold">{{ $book->very_short_title }}</span><br />
+                                    @if($book->creator)
+                                        <span class="author-font">By {{ $book->creator->name }}</span><br />
+                                    @else
+                                        <span class="author-font">By {{ \App\Constants\GlobalConstants::ADMIN }}</span><br />
+                                    @endif
+
+                                    @if($book->price)
+                                        UGX {{ number_format($book->price) }}/- <span class="author-font">(Paid)</span>
+                                    @else
+                                        <span class="bold paid_color">Free</span>
+                                    @endif
+                                </a>
+                            @elseif(!$book->price)
+                                <a href="{{ route('student.books.show', $book) }}" style="text-decoration: none" class="title-font">
+                                    <span class="bold">{{ $book->very_short_title }}</span><br />
+                                    @if($book->creator)
+                                        <span class="author-font">By {{ $book->creator->name }}</span><br />
+                                    @else
+                                        <span class="author-font">By {{ \App\Constants\GlobalConstants::ADMIN }}</span><br />
+                                    @endif
+
+                                    @if($book->price)
+                                        <span class="bold">UGX {{ number_format($book->price) }}/-</span>
+                                    @else
+                                        <span class="bold paid_color">Free</span>
+                                    @endif
+                                </a>
+                            @else
                                 <span class="bold">{{ $book->very_short_title }}</span><br />
                                 @if($book->creator)
                                     <span class="author-font">By {{ $book->creator->name }}</span><br />
@@ -56,28 +71,15 @@
                                 @else
                                     <span class="bold paid_color">Free</span>
                                 @endif
-                            </a>
-                        @else
-                            <span class="bold">{{ $book->very_short_title }}</span><br />
-                            @if($book->creator)
-                                <span class="author-font">By {{ $book->creator->name }}</span><br />
-                            @else
-                                <span class="author-font">By {{ \App\Constants\GlobalConstants::ADMIN }}</span><br />
                             @endif
-
-                            @if($book->price)
-                                <span class="bold">UGX {{ number_format($book->price) }}/-</span>
-                            @else
-                                <span class="bold paid_color">Free</span>
-                            @endif
-                        @endif
-                        <div class="mt-2">
-                            <livewire:buy-book :book="$book" :key="$book->id" />
+                            <div class="mt-2">
+                                <livewire:buy-book :book="$book" :key="$book->id" />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endif
     @empty
         <div class="col-lg-12 col-md-12 col-sm-12 d-flex justify-content-center mt-5">
             <p>The book(s) you are looking for was not found. </p>
