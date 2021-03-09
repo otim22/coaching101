d95b68de38b54a0d35994b51a20cb03b099da29c
Otim Fredrick <fredrickot@gmail.com>
2021-03-09 17:25:10 +0300

    refixes merge conflicts

--- routes/web.php
+++ routes/web.php
@@ -84,47 +84,53 @@ Auth::routes(['verify' => true]);
 
 Route::get('/teacher/onBoard', [SubjectController::class, 'onBoard'])->name('subjects.onBoard');
 Route::middleware('auth')->group(function() {
-    Route::get('/cart/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
     Route::get('/cart/{response?}', [CartController::class, 'index'])->name('cart.index');
 
     Route::post( '/pay', [PaymentController::class, 'initialize'])->name('pay');
     Route::post('/rave/callback', [PaymentController::class, 'callback'])->name('callback');
 
-    Route::prefix('teacher')
-            ->group(function() {
-                Route::get('/starter', [SubjectController::class, 'starter'])->name('subjects.starter');
-                Route::get('/onBoard', [SubjectController::class, 'onBoard'])->name('subjects.onBoard');
-                Route::post('/captureRole', [SubjectController::class, 'captureRole'])->name('subjects.captureRole');
-                Route::get('/manage/subjects', [SubjectController::class, 'index'])->name('manage.subjects')->middleware('teacher');
-                Route::get('/subjects', [SubjectController::class, 'create'])->name('subjects.create');
-                Route::post('/subjects', [SubjectController::class, 'store'])->name('subjects');
-                Route::get('/subjects/{subject}', [SubjectController::class, 'show'])->name('subjects.show');
-                Route::get('/subjects/{subject}/edit', [SubjectController::class, 'edit'])->name('subjects.edit');
-                Route::patch('/subjects/{subject}/update', [SubjectController::class, 'update'])->name('subjects.update');
-                Route::delete('/subjects/{subject}/destroy', [SubjectController::class, 'destroy'])->name('subjects.destroy');
+    Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+    Route::prefix('teacher')->group(function() {
+        Route::get('/starter', [SubjectController::class, 'starter'])->name('subjects.starter');
+        Route::post('/captureRole', [SubjectController::class, 'captureRole'])->name('subjects.captureRole');
+        Route::get('/manage/subjects', [SubjectController::class, 'index'])->name('manage.subjects')->middleware('teacher');
+        Route::get('/subjects', [SubjectController::class, 'create'])->name('subjects.create');
+        Route::post('/subjects', [SubjectController::class, 'store'])->name('subjects');
+        Route::get('/subjects/{subject}', [SubjectController::class, 'show'])->name('subjects.show');
+        Route::get('/subjects/{subject}/edit', [SubjectController::class, 'edit'])->name('subjects.edit');
+        Route::patch('/subjects/{subject}/update', [SubjectController::class, 'update'])->name('subjects.update');
+        Route::delete('/subjects/{subject}/destroy', [SubjectController::class, 'destroy'])->name('subjects.destroy');
 
-                Route::get('/subjects/{subject}/audiences', [AudienceController::class, 'index']);
-                Route::get('/subjects/{subject}/audiences', [AudienceController::class, 'create']);
-                Route::post('/subjects/{subject}/audiences', [AudienceController::class, 'store'])->name('audiences');
-                Route::get('/subjects/{subject}/audiences/edit', [AudienceController::class, 'edit'])->name('audiences.edit');
-                Route::patch('/subjects/{subject}/audiences/update', [AudienceController::class, 'update'])->name('audiences.update');
+        Route::resource('/books', 'TeacherBookController')->except(['index']);
+        Route::get('/books', [TeacherBookController::class, 'index'])->name('teacher.books');
+        Route::resource('/notes', 'TeacherNoteController')->except(['index']);
+        Route::get('/notes', [TeacherNoteController::class, 'index'])->name('teacher.notes');
+        Route::resource('/pastpapers', 'TeacherPastpaperController')->except(['index']);
+        Route::get('/pastpapers', [TeacherPastpaperController::class, 'index'])->name('teacher.pastpapers');
 
-                Route::get('/subjects/{subject}/messages', [MessageController::class, 'index']);
-                Route::get('/subjects/{subject}/messages', [MessageController::class, 'create']);
-                Route::post('/subjects/{subject}/messages', [MessageController::class, 'store'])->name('messages');
-                Route::get('/subjects/{subject}/messages/edit', [MessageController::class, 'edit'])->name('messages.edit');
-                Route::patch('/subjects/{subject}/messages/update', [MessageController::class, 'update'])->name('messages.update');
+        Route::get('/subjects/{subject}/audiences', [AudienceController::class, 'index']);
+        Route::get('/subjects/{subject}/audiences', [AudienceController::class, 'create']);
+        Route::post('/subjects/{subject}/audiences', [AudienceController::class, 'store'])->name('audiences');
+        Route::get('/subjects/{subject}/audiences/edit', [AudienceController::class, 'edit'])->name('audiences.edit');
+        Route::patch('/subjects/{subject}/audiences/update', [AudienceController::class, 'update'])->name('audiences.update');
 
-                Route::get('/subjects/{subject}/topics', [TopicController::class, 'index'])->name('topics.index');
-                Route::get('/subjects/{subject}/topics', [TopicController::class, 'create'])->name('topics.create');
-                Route::get('/subjects/{subject}/topics/{topic}', [TopicController::class, 'show'])->name('topics.show');
-                Route::post('/subjects/{subject}/topics', [TopicController::class, 'store'])->name('topics');
-                Route::get('/subjects/{subject}/topics/{topic}/edit', [TopicController::class, 'edit'])->name('topics.edit');
-                Route::patch('/subjects/{subject}/topics/{topic}/update', [TopicController::class, 'update'])->name('topics.update');
+        Route::get('/subjects/{subject}/messages', [MessageController::class, 'index']);
+        Route::get('/subjects/{subject}/messages', [MessageController::class, 'create']);
+        Route::post('/subjects/{subject}/messages', [MessageController::class, 'store'])->name('messages');
+        Route::get('/subjects/{subject}/messages/edit', [MessageController::class, 'edit'])->name('messages.edit');
+        Route::patch('/subjects/{subject}/messages/update', [MessageController::class, 'update'])->name('messages.update');
 
-                Route::get('/manage/performances', [PerformanceController::class, 'index'])->name('manage.performances');
-                Route::get('/manage/resources', [ResourceController::class, 'index'])->name('manage.resources');
-                Route::get('/manage/tools', [ToolController::class, 'index'])->name('manage.tools');
+        Route::get('/subjects/{subject}/topics', [TopicController::class, 'index'])->name('topics.index');
+        Route::get('/subjects/{subject}/topics', [TopicController::class, 'create'])->name('topics.create');
+        Route::get('/subjects/{subject}/topics/{topic}', [TopicController::class, 'show'])->name('topics.show');
+        Route::post('/subjects/{subject}/topics', [TopicController::class, 'store'])->name('topics');
+        Route::get('/subjects/{subject}/topics/{topic}/edit', [TopicController::class, 'edit'])->name('topics.edit');
+        Route::patch('/subjects/{subject}/topics/{topic}/update', [TopicController::class, 'update'])->name('topics.update');
+
+        Route::get('/manage/performances', [PerformanceController::class, 'index'])->name('manage.performances');
+        Route::get('/get-more-subjects-for-teacher-performance', [PerformanceController::class, 'getMoreSubjectsTeacherPerforamce'])->name('get-more-subjects-for-teacher-performance');
+        Route::get('/manage/resources', [ResourceController::class, 'index'])->name('manage.resources');
+        Route::get('/manage/tools', [ToolController::class, 'index'])->name('manage.tools');
     });
 
     Route::namespace('Admin')->prefix('admin')->name('admin.')->middleware('admin')->group(function() {
