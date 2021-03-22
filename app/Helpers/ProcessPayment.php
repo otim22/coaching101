8324ba499a74e65efd9b2d56ea245a25925e9a6a
Otim Fredrick <fredrickot@gmail.com>
2021-03-22 16:26:48 +0300

    minor refactpr around cart

--- app/Helpers/ProcessPayment.php
+++ app/Helpers/ProcessPayment.php
@@ -22,7 +22,7 @@ class ProcessPayment
             $this->url . '?type=card', [ 'client' => $postData ] );
     }
 
-    protected function encrypt3Des($data, $key){
+    protected function encrypt3Des($data, $key) {
         $encData = openssl_encrypt($data, 'DES-EDE3', $key, OPENSSL_RAW_DATA);
         return base64_encode($encData);
     }
