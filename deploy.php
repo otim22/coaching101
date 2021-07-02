7644c287b6eee43ca8ce231550dfbb581a45f3a8
Otim Fredrick <fredrickot@gmail.com>
2021-07-02 12:57:46 +0300

    adds seeding after migration in deploy file

--- deploy.php
+++ deploy.php
@@ -42,4 +42,4 @@ after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
 
-before('deploy:symlink', 'artisan:migrate');
+before('deploy:symlink', 'artisan:migrate --seed');
