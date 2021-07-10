74b178e101c5f951309c579823b84611ddabffec
Otim Fredrick <fredrickot@gmail.com>
2021-07-11 01:38:36 +0300

    sets identity to deployer file

--- deploy.php
+++ deploy.php
@@ -12,8 +12,7 @@ set('php_fpm_version', '7.4');
 
 host('production')
     ->set('remote_user', 'lapwony')
-    ->configFile('~/.ssh/config')
-    ->identityFile('~/.ssh/lapwonyrkey')
+    ->set('identityFile', '~/.ssh/lapwonyrkey')
     ->set('branch', 'master')
     ->set('hostname', '167.71.47.82')
     ->set('deploy_path', '/var/www/oncloudlearning.com');
