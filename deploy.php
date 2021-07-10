caf637face365e357dbd222ac93a9b0ed88f6af6
Otim Fredrick <fredrickot@gmail.com>
2021-07-11 00:22:05 +0300

    modifies deployer file

--- deploy.php
+++ deploy.php
@@ -1,171 +1,73 @@
 <?php
+
 namespace Deployer;
 
 require 'recipe/laravel.php';
+require 'contrib/php-fpm.php';
+require 'contrib/npm.php';
 
-// Project name
 set('application', 'Coaching101');
-
-// Project repository
 set('repository', 'git@github.com:otim22/coaching101.git');
+set('php_fpm_version', '7.4');
 
-// [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true);
+host('167.71.47.82')
+    ->set('remote_user', 'root')
+    ->set('hostname', 'oncloudlearning.com')
+    ->set('deploy_path', '/var/www/{{hostname}}');
 
-// Shared files/dirs between deploys
-add('shared_files', []);
-add('shared_dirs', []);
-
-// Writable dirs by web server
-add('writable_dirs', []);
-
-// Helper Tasks
-
-desc('Disable maintenance mode');
-task('artisan:up', function () {
-    $output = run('if [ -f {{deploy_path}}/current/artisan ]; then {{bin/php}} {{deploy_path}}/current/artisan up; fi');
-    writeln('<info>' . $output . '</info>');
-});
-
-desc('Enable maintenance mode');
-task('artisan:down', function () {
-    $output = run('if [ -f {{deploy_path}}/current/artisan ]; then {{bin/php}} {{deploy_path}}/current/artisan down; fi');
-    writeln('<info>' . $output . '</info>');
-});
-
-desc('Execute artisan migrate');
-task('artisan:migrate', function () {
-    run('{{bin/php}} {{release_path}}/artisan migrate --force');
-})->once();
-
-desc('Execute artisan migrate:fresh');
-task('artisan:migrate:fresh', function () {
-    run('{{bin/php}} {{release_path}}/artisan migrate:fresh --force');
-})->once();
-
-desc('Execute artisan migrate:rollback');
-task('artisan:migrate:rollback', function () {
-    $output = run('{{bin/php}} {{release_path}}/artisan migrate:rollback --force');
-    writeln('<info>' . $output . '</info>');
-})->once();
-
-desc('Execute artisan migrate:status');
-task('artisan:migrate:status', function () {
-    $output = run('{{bin/php}} {{release_path}}/artisan migrate:status');
-    writeln('<info>' . $output . '</info>');
-})->once();
-
-desc('Execute artisan db:seed');
-task('artisan:db:seed', function () {
-    $output = run('{{bin/php}} {{release_path}}/artisan db:seed --force');
-    writeln('<info>' . $output . '</info>');
-})->once();
-
-desc('Execute artisan migrate:fresh --seed');
-task('artisan:migrate:fresh:seed', function () {
-    $output = run('{{bin/php}} {{release_path}}/artisan migrate:fresh --seed');
-    writeln('<info>' . $output . '</info>');
-})->once();
-
-desc('Execute artisan cache:clear');
-task('artisan:cache:clear', function () {
-    run('{{bin/php}} {{release_path}}/artisan cache:clear');
-});
-
-desc('Execute artisan config:cache');
-task('artisan:config:cache', function () {
-    run('{{bin/php}} {{release_path}}/artisan config:cache');
-});
-
-desc('Execute artisan route:cache');
-task('artisan:route:cache', function () {
-    run('{{bin/php}} {{release_path}}/artisan route:cache');
-});
-
-desc('Execute artisan view:clear');
-task('artisan:view:clear', function () {
-    run('{{bin/php}} {{release_path}}/artisan view:clear');
-});
-
-desc('Execute artisan optimize');
-task('artisan:optimize', function () {
-    $deprecatedVersion = 5.5;
-    $currentVersion = get('laravel_version');
-    if (version_compare($currentVersion, $deprecatedVersion, '<')) {
-        run('{{bin/php}} {{release_path}}/artisan optimize');
-    }
-});
-
-desc('Execute artisan queue:restart');
-task('artisan:queue:restart', function () {
-    run('{{bin/php}} {{release_path}}/artisan queue:restart');
-});
-
-desc('Execute artisan storage:link');
-task('artisan:storage:link', function () {
-    $needsVersion = 5.3;
-    $currentVersion = get('laravel_version');
-    if (version_compare($currentVersion, $needsVersion, '>=')) {
-        run('{{bin/php}} {{release_path}}/artisan storage:link');
-    }
-});
-
-/**
- * Task deploy:public_disk support the public disk.
- * To run this task automatically, please add below line to your deploy.php file
- *
- *     before('deploy:symlink', 'deploy:public_disk');
- *
- * @see https://laravel.com/docs/master/filesystem#the-public-disk
- */
-
-desc('Make symlink for public disk');
-task('deploy:public_disk', function () {
-    // Remove from source.
-    run('if [ -d $(echo {{release_path}}/public/storage) ]; then rm -rf {{release_path}}/public/storage; fi');
-    // Create shared dir if it does not exist.
-    run('mkdir -p {{deploy_path}}/shared/storage/app/public');
-    // Symlink shared dir to release dir
-    run('{{bin/symlink}} {{deploy_path}}/shared/storage/app/public {{release_path}}/public/storage');
-});
-
-// Hosts
-
-host('174.138.32.252')
-    ->user('deployer')
-    ->identityFile('~/.ssh/deployerkey')
-    ->set('branch', 'master')
-    ->set('deploy_path', '/var/www/html/coaching101')
-    ->set('http_user', 'www-data')
-    ->set('writable_mode', 'chmod')
-    ->set('use_relative_symlink', '0');
-
-// Tasks
-
-desc('Deploy your project');
-task('deploy', [
+task('dev', [
     'deploy:info',
     'deploy:prepare',
     'deploy:lock',
     'deploy:release',
-    'deploy:update_code',
-    'deploy:shared',
     'deploy:vendors',
-    // 'deploy:writable',
     'artisan:storage:link',
-    'artisan:view:clear',
+    'artisan:view:cache',
     'artisan:cache:clear',
     'artisan:config:cache',
     'artisan:optimize',
-    'deploy:symlink',
+    'artisan:migrate',
+    'artisan:db:seed',
+    'composer:install',
+    'npm:install',
+    'npm:run:prod',
+    'deploy:publish',
+    'php-fpm:reload',
     'deploy:unlock',
     'cleanup',
     'success'
 ]);
 
-// [Optional] if deploy fails automatically unlock.
+task('prod', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:vendors',
+    'artisan:storage:link',
+    'artisan:view:cache',
+    'artisan:cache:clear',
+    'artisan:config:cache',
+    'artisan:optimize',
+    'artisan:migrate',
+    'composer:install',
+    'npm:install',
+    'npm:run:prod',
+    'deploy:publish',
+    'php-fpm:reload',
+    'deploy:unlock',
+    'cleanup',
+    'success'
+]);
+
+task('composer:install', function () {
+    cd('{{release_path}}');
+    run('composer install');
+});
+
+task('npm:run:prod', function () {
+    cd('{{release_path}}');
+    run('npm run prod');
+});
+
 after('deploy:failed', 'deploy:unlock');
-
-// Migrate database before symlink new release.
-
-after('deploy:vendors', 'artisan:migrate');
