5b278e700fb70d9e267392fa3a36f414cafe5351
Otim Fredrick <fredrickot@gmail.com>
2021-07-13 09:14:29 +0300

    modifies deployer file

--- deploy.php
+++ deploy.php
@@ -10,29 +10,19 @@ set('application', 'Coaching101');
 set('repository', 'git@github.com:otim22/coaching101.git');
 set('php_fpm_version', '7.4');
 
-set('git_tty', true);
-
 host('prod')
     ->set('remote_user', 'lapwony')
     ->set('identity_file', '~/.ssh/lapwonykey')
     ->set('branch', 'master')
     ->set('hostname', '167.71.47.82')
-    ->set('deploy_path', '/var/www/oncloudlearning.com')
-    ->set('http_user', 'www-data')
-    ->set('writable_mode', 'chmod');
+    ->set('deploy_path', '/var/www/oncloudlearning.com');
 
 task('dev', [
-    'deploy:info',
     'deploy:prepare',
-    'deploy:release',
-    'deploy:update_code',
     'deploy:vendors',
     'artisan:storage:link',
     'artisan:view:cache',
-    'artisan:cache:clear',
     'artisan:config:cache',
-    'artisan:optimize',
-    'artisan:route:clear',
     'artisan:migrate',
     'npm:install',
     'npm:run:prod',
@@ -58,7 +48,7 @@ task('dev', [
 // ]);
 
 task('npm:run:prod', function () {
-    cd('{{release_path}}');
+    cd('{{release_or_current_path}}');
     run('npm run prod');
 });
 
