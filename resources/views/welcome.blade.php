1e167e80b3848309e692cbbe942a7d0ac2e1172a
Otim Fredrick <fredrickot@gmail.com>
2021-07-29 16:39:00 +0300

    reduces image size

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,7 +41,7 @@
                         <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
                     </svg>
                 </div> -->
-                <img src="{{ asset('images/class2.png') }}" alt="" class="pr-4" width="30%">
+                <img src="{{ asset('images/class2.png') }}" alt="" class="pr-4" width="30%" height="auto">
                 <div class="bottom-spacing mt-2">
                     <h5 class="bold">Online Classes</h5>
                     <p>Discover varied topics</p>
@@ -54,7 +54,7 @@
                         <path fill-rule="evenodd" d="M15.854 2.146a.5.5 0 010 .708l-3 3a.5.5 0 01-.708 0l-1.5-1.5a.5.5 0 01.708-.708L12.5 4.793l2.646-2.647a.5.5 0 01.708 0z" clip-rule="evenodd"/>
                     </svg>
                 </div> -->
-                <img src="{{ asset('images/expert2.png') }}" alt="" class="pr-4" width="30%">
+                <img src="{{ asset('images/expert2.png') }}" alt="" class="pr-4" width="30%" height="auto">
                 <div class="bottom-spacing mt-2">
                     <h5 class="bold">Expert Teachers</h5>
                     <p>Connect with right teachers</p>
@@ -68,7 +68,7 @@
                         <path fill-rule="evenodd" d="M7.5 3a.5.5 0 01.5.5v5.21l3.248 1.856a.5.5 0 01-.496.868l-3.5-2A.5.5 0 017 9V3.5a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
                     </svg>
                 </div> -->
-                <img src="{{ asset('images/timetable.png') }}" alt="" class="pr-4" width="30%">
+                <img src="{{ asset('images/timetable.png') }}" alt="" class="pr-4" width="30%" height="auto">
                 <div class="mt-2">
                     <h5 class="bold">Access Time </h5>
                     <p>Learn on your schedule</p>
