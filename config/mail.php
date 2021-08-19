7f4f42b4b9064c3cbc42214ac1f174d8282a193c
Fredrick Otim <fredrickot@gmail.com>
2021-08-19 11:01:53 +0300

    adds allow self sign to mail.php

--- config/mail.php
+++ config/mail.php
@@ -70,6 +70,15 @@ return [
         'array' => [
             'transport' => 'array',
         ],
+
+        'stream' => [
+            'ssl' => [
+                'allow_self_signed' => true,
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+            ],
+        ],
+        
     ],
 
     /*
